41b47aa99966e4f59dca9b9d9fd16311d42ac230
Manfred Steyer <manfred.steyer@gmx.net>
2024-01-18 10:16:13 +0100

    chore: fix linting errors

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 
 const maxRatingInCheatMode = 500;
 
