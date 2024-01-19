0886131ac7d2e24a8b18215620689850b1fd8466
Manfred Steyer <manfred.steyer@gmx.net>
2024-01-19 22:39:47 +0100

    add signal store

--- src/app/data/dessert.store.ts
+++ src/app/data/dessert.store.ts
@@ -1,66 +1,60 @@
-import { Injectable, computed, inject, signal } from '@angular/core';
+import { computed, inject } from '@angular/core';
 import { Dessert } from './dessert';
 import { DessertFilter } from './dessert-filter';
 import { DessertService } from './dessert.service';
 import { RatingService } from './rating.service';
+import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
 
-@Injectable({ providedIn: 'root' })
-export class DessertStore {
-
-    #dessertService = inject(DessertService);
-
-    // When working with lightweight stores
-    // rating could be put in a store of itself.
-    #ratingService = inject(RatingService);
-
-    #state = signal({
+export const DessertStore = signalStore(
+    { providedIn: 'root' },
+    withState({
         filter: {
             originalName: '',
             englishName: '',
         },
         desserts: [] as Dessert[],
-    });
-
-    readonly originalName = computed(() => this.#state().filter.originalName);
-    readonly englishName = computed(() => this.#state().filter.englishName);
-
-    readonly desserts = computed(() => this.#state().desserts);
-
-    readonly maxRating = computed(() => this.desserts().reduce(
-        (acc, d) => Math.max(acc, d.rating),
-        0
-    ));
-
-    updateFilter(filter: DessertFilter): void {
-        this.#state.update(state => ({ ...state, filter }));
-    }
-
-    async loadDesserts(): Promise<void> {
-        const desserts = await this.#dessertService.findPromise(this.#state().filter);
-        this.#state.update(state => ({ ...state, desserts }));
-    }
-
-    async loadRatings(): Promise<void> {
-        const ratings = await this.#ratingService.loadExpertRatings();
-
-        this.#state.update(state => ({
-            ...state,
-            desserts: state.desserts.map(
-                d => ratings[d.id] ?
-                    { ...d, rating: ratings[d.id] } :
-                    d
-            )
-        }));
-    }
-
-    updateRating(id: number, rating: number): void {
-        this.#state.update(state => ({
-            ...state,
-            desserts: state.desserts.map(
-                d => (d.id === id) ?
-                    { ...d, rating: rating } :
-                    d
-            )
-        }));
-    }
-}
+    }),
+    withComputed((store) => ({
+        maxRating: computed(() => store.desserts().reduce(
+            (acc, d) => Math.max(acc, d.rating),
+            0
+        ))
+    })),
+    withMethods((
+        store,
+        dessertService = inject(DessertService),
+        ratingService = inject(RatingService)
+    ) => ({
+        updateFilter(filter: DessertFilter): void {
+            patchState(store, { filter });
+        },
+    
+        async loadDesserts(): Promise<void> {
+            const desserts = await dessertService.findPromise(store.filter());
+            patchState(store, { desserts });
+        },
+    
+        async loadRatings(): Promise<void> {
+            const ratings = await ratingService.loadExpertRatings();
+    
+            patchState(store, state => ({
+                desserts: state.desserts.map(
+                    d => ratings[d.id] ?
+                        { ...d, rating: ratings[d.id] } :
+                        d
+                )
+            }));
+        },
+    
+        updateRating(id: number, rating: number): void {
+            patchState(store, state => ({
+                ...state,
+                desserts: state.desserts.map(
+                    d => (d.id === id) ?
+                        { ...d, rating: rating } :
+                        d
+                )
+            }));
+        }
+    }))
+);
