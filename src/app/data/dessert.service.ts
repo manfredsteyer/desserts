d21fdb76da80fa9ff83d8d73417f86019e95f089
Manfred Steyer <manfred.steyer@gmx.net>
2024-02-23 03:29:28 +0100

    run prettier

--- src/app/data/dessert.service.ts
+++ src/app/data/dessert.service.ts
@@ -8,28 +8,37 @@ const dataFile = '/assets/desserts.json';
 
 @Injectable({ providedIn: 'root' })
 export class DessertService {
+  #http = inject(HttpClient);
 
-    #http = inject(HttpClient);
+  find(filter: DessertFilter): Observable<Dessert[]> {
+    return this.#http
+      .get<Dessert[]>(dataFile)
+      .pipe(
+        map((result) =>
+          result.filter(
+            (d) =>
+              d.originalName
+                .toLowerCase()
+                .includes(filter.originalName.toLowerCase()) &&
+              d.englishName
+                .toLowerCase()
+                .includes(filter.englishName.toLowerCase()),
+          ),
+        ),
+      );
+  }
 
-    find(filter: DessertFilter): Observable<Dessert[]> {
-        return this.#http.get<Dessert[]>(dataFile).pipe(
-            map(result => result.filter(
-                d => d.originalName.toLowerCase().includes(filter.originalName.toLowerCase())
-                    && d.englishName.toLowerCase().includes(filter.englishName.toLowerCase())))
-        );
-    }
+  findPromise(filter: DessertFilter): Promise<Dessert[]> {
+    return lastValueFrom(this.find(filter));
+  }
 
-    findPromise(filter: DessertFilter): Promise<Dessert[]> {
-        return lastValueFrom(this.find(filter));
-    }
+  findById(id: number): Observable<Dessert[]> {
+    return this.#http
+      .get<Dessert[]>(dataFile)
+      .pipe(map((result) => result.filter((d) => d.id == id)));
+  }
 
-    findById(id: number): Observable<Dessert[]> {
-        return this.#http.get<Dessert[]>(dataFile).pipe(
-            map(result => result.filter(d => d.id == id))
-        );
-    }
-
-    findPromiseById(id: number): Promise<Dessert[]> {
-        return lastValueFrom(this.findById(id));
-    }
+  findPromiseById(id: number): Promise<Dessert[]> {
+    return lastValueFrom(this.findById(id));
+  }
 }
