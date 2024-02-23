fde8a8d46424c1138a160add9f50589dae11d7ce
Manfred Steyer <manfred.steyer@gmx.net>
2024-02-23 18:25:08 +0100

    format

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,8 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
