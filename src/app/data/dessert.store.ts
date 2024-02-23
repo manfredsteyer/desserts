803881db344795f547f2707af41c31f54d3b1e4f
Manfred Steyer <manfred.steyer@gmx.net>
2024-02-23 03:31:17 +0100

    run prettier

--- src/app/data/dessert.store.ts
+++ src/app/data/dessert.store.ts
@@ -3,59 +3,72 @@ import { Dessert } from './dessert';
 import { DessertFilter } from './dessert-filter';
 import { DessertService } from './dessert.service';
 import { DessertIdToRatingMap, RatingService } from './rating.service';
-import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from '@ngrx/signals';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withHooks,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { toRated } from './to-rated';
 import { debounceTime, filter, pipe, switchMap, tap } from 'rxjs';
 
 export const DessertStore = signalStore(
-    { providedIn: 'root' },
-    withState({
-        filter: {
-            originalName: '',
-            englishName: 'Cake',
-        },
-        ratings: {} as DessertIdToRatingMap,
-        desserts: [] as Dessert[],
-    }),
-    withComputed((store) => ({
-        ratedDesserts: computed(() => toRated(store.desserts(), store.ratings()))
-    })),
-    withMethods((
-        store,
-        dessertService = inject(DessertService),
-        ratingService = inject(RatingService)
+  { providedIn: 'root' },
+  withState({
+    filter: {
+      originalName: '',
+      englishName: 'Cake',
+    },
+    ratings: {} as DessertIdToRatingMap,
+    desserts: [] as Dessert[],
+  }),
+  withComputed((store) => ({
+    ratedDesserts: computed(() => toRated(store.desserts(), store.ratings())),
+  })),
+  withMethods(
+    (
+      store,
+      dessertService = inject(DessertService),
+      ratingService = inject(RatingService),
     ) => ({
-        updateFilter(filter: DessertFilter): void {
-            patchState(store, { filter });
-        },
-        async loadDesserts(): Promise<void> {
-            const desserts = await dessertService.findPromise(store.filter());
-            patchState(store, { desserts });
-        },
-        async loadRatings(): Promise<void> {
-            const ratings = await ratingService.loadExpertRatings();
-            patchState(store, { ratings });
-        },
-        updateRating(id: number, rating: number): void {
-            patchState(store, state => ({
-                ratings: {
-                    ...state.ratings,
-                    [id]: rating
-                }
-            }));
-        },
-        loadDessertsByFilter: rxMethod<DessertFilter>(pipe(
-            filter(f => f.originalName.length >= 3 || f.englishName.length >= 3),
-            debounceTime(300),
-            switchMap(f => dessertService.find(f)),
-            tap(desserts => patchState(store, { desserts }))
-        ))
-    })),
-    withHooks({
-        onInit(store) {
-            const filter = store.filter;
-            store.loadDessertsByFilter(filter);
-        }
-    })
+      updateFilter(filter: DessertFilter): void {
+        patchState(store, { filter });
+      },
+      async loadDesserts(): Promise<void> {
+        const desserts = await dessertService.findPromise(store.filter());
+        patchState(store, { desserts });
+      },
+      async loadRatings(): Promise<void> {
+        const ratings = await ratingService.loadExpertRatings();
+        patchState(store, { ratings });
+      },
+      updateRating(id: number, rating: number): void {
+        patchState(store, (state) => ({
+          ratings: {
+            ...state.ratings,
+            [id]: rating,
+          },
+        }));
+      },
+      loadDessertsByFilter: rxMethod<DessertFilter>(
+        pipe(
+          filter(
+            (f) => f.originalName.length >= 3 || f.englishName.length >= 3,
+          ),
+          debounceTime(300),
+          switchMap((f) => dessertService.find(f)),
+          tap((desserts) => patchState(store, { desserts })),
+        ),
+      ),
+    }),
+  ),
+  withHooks({
+    onInit(store) {
+      const filter = store.filter;
+      store.loadDessertsByFilter(filter);
+    },
+  }),
 );
