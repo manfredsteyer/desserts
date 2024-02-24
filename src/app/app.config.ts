ccae84a562d044042e7744dca95330655f60450d
Manfred Steyer <manfred.steyer@gmx.net>
2024-02-24 11:33:25 +0100

    clean up blink effect and zone-less

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,9 @@
-import {
-  ApplicationConfig,
-  ɵprovideZonelessChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    ɵprovideZonelessChangeDetection(),
-  ],
+  providers: [provideRouter(routes), provideHttpClient()],
 };
