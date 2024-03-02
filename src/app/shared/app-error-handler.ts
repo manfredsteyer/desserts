15108d4655debd4512b9b0b4ae70bd55198a35f5
Manfred Steyer <manfred.steyer@gmx.net>
2024-03-02 20:03:14 +0100

    add global error handler

---
+++ src/app/shared/app-error-handler.ts
@@ -0,0 +1,27 @@
+import { EnvironmentProviders, ErrorHandler, Injectable, NgZone, inject, makeEnvironmentProviders } from '@angular/core';
+import { ToastService } from './toast';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class AppErrorHandler extends ErrorHandler {
+    #toastService = inject(ToastService);
+    #zone = inject(NgZone);
+
+    override handleError(error: unknown): void {
+        this.#zone.run(() => {
+            if (error instanceof HttpErrorResponse) {
+                this.#toastService.show('Error during backend communication.');
+            }
+            else {
+                this.#toastService.show('Unexpected error.');
+            }
+        })
+        super.handleError(error);
+    }
+}
+
+export function provideAppErrorHandler(): EnvironmentProviders {
+    return makeEnvironmentProviders([
+        { provide: ErrorHandler, useClass: AppErrorHandler }
+    ]);
+}
