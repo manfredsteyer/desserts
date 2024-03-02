7aac3a54b27fbd8b09af2a93ec5d5d97d585a0d8
Manfred Steyer <manfred.steyer@gmx.net>
2024-03-02 20:03:14 +0100

    add global error handler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,13 @@ import { provideRouter } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideAppErrorHandler } from './shared/app-error-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
+    provideAppErrorHandler(),
     ɵprovideZonelessChangeDetection(),
   ],
 };
