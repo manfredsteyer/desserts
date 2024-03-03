e24c2b70168307bb93e2f24b4b52f297bd528f32
Manfred Steyer <manfred.steyer@gmx.net>
2024-03-03 20:30:24 +0100

    format code

--- src/app/data/dessert.store.ts
+++ src/app/data/dessert.store.ts
@@ -1,4 +1,5 @@
 import { computed, inject } from '@angular/core';
+import { tapResponse } from '@ngrx/operators';
 import {
   patchState,
   signalStore,
@@ -9,13 +10,12 @@ import {
 } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { debounceTime, filter, pipe, switchMap, tap } from 'rxjs';
+import { ToastService } from '../shared/toast';
 import { Dessert } from './dessert';
 import { DessertFilter } from './dessert-filter';
 import { DessertService } from './dessert.service';
 import { DessertIdToRatingMap, RatingService } from './rating.service';
 import { toRated } from './to-rated';
-import { tapResponse } from '@ngrx/operators';
-import { ToastService } from '../shared/toast';
 
 export const DessertStore = signalStore(
   { providedIn: 'root' },
@@ -27,36 +27,40 @@ export const DessertStore = signalStore(
     loading: false,
     desserts: [] as Dessert[],
   }),
-  withMethods((
-    store,
-    dessertService = inject(DessertService),
-    toastService = inject(ToastService)
-  ) => ({
-    updateFilter(filter: DessertFilter): void {
-      patchState(store, { filter });
-    },
-    loadDessertsByFilter: rxMethod<DessertFilter>(
-      pipe(
-        filter(
-          (f) => f.originalName.length >= 3 || f.englishName.length >= 3,
+  withMethods(
+    (
+      store,
+      dessertService = inject(DessertService),
+      toastService = inject(ToastService),
+    ) => ({
+      updateFilter(filter: DessertFilter): void {
+        patchState(store, { filter });
+      },
+      loadDessertsByFilter: rxMethod<DessertFilter>(
+        pipe(
+          filter(
+            (f) => f.originalName.length >= 3 || f.englishName.length >= 3,
+          ),
+          debounceTime(300),
+          tap(() => patchState(store, { loading: true })),
+          switchMap((f) =>
+            dessertService.find(f).pipe(
+              tapResponse({
+                next: (desserts) => {
+                  patchState(store, { desserts, loading: false });
+                },
+                error: (error) => {
+                  toastService.show('Error loading desserts!');
+                  console.error(error);
+                  patchState(store, { loading: false });
+                },
+              }),
+            ),
+          ),
         ),
-        debounceTime(300),
-        tap(() => patchState(store, { loading: true })),
-        switchMap((f) => dessertService.find(f).pipe(
-          tapResponse({
-            next: (desserts) => {
-              patchState(store, { desserts, loading: false });
-            },
-            error: (error) => {
-              toastService.show('Error loading desserts!');
-              console.error(error);
-              patchState(store, { loading: false })
-            },
-          })
-        )),
       ),
-    ),
-  })),
+    }),
+  ),
   withHooks({
     onInit(store) {
       const filter = store.filter;
@@ -74,32 +78,34 @@ export const DessertStore = signalStore(
   withComputed((store) => ({
     ratedDesserts: computed(() => toRated(store.desserts(), store.ratings())),
   })),
-  withMethods((
-    store, 
-    ratingService = inject(RatingService),
-    toastService = inject(ToastService)
-  ) => ({
-    loadRatings(): void {
-      patchState(store, { loading: true });
-    
-      ratingService.loadExpertRatings().subscribe({
-        next: (ratings) => {
-          patchState(store, { ratings, loading: false });
-        },
-        error: (error) => {
-          patchState(store, { loading: false });
-          toastService.show('Error loading ratings!');
-          console.error(error);
-        }
-      });
-    },
-    updateRating(id: number, rating: number): void {
-      patchState(store, (state) => ({
-        ratings: {
-          ...state.ratings,
-          [id]: rating,
-        },
-      }));
-    },
-  })),
+  withMethods(
+    (
+      store,
+      ratingService = inject(RatingService),
+      toastService = inject(ToastService),
+    ) => ({
+      loadRatings(): void {
+        patchState(store, { loading: true });
+
+        ratingService.loadExpertRatings().subscribe({
+          next: (ratings) => {
+            patchState(store, { ratings, loading: false });
+          },
+          error: (error) => {
+            patchState(store, { loading: false });
+            toastService.show('Error loading ratings!');
+            console.error(error);
+          },
+        });
+      },
+      updateRating(id: number, rating: number): void {
+        patchState(store, (state) => ({
+          ratings: {
+            ...state.ratings,
+            [id]: rating,
+          },
+        }));
+      },
+    }),
+  ),
 );
