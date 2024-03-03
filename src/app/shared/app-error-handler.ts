64daae123ea963659bcb67b7f3a9e6974936e0eb
Manfred Steyer <manfred.steyer@gmx.net>
2024-03-03 20:12:10 +0100

    format code

--- src/app/shared/app-error-handler.ts
+++ src/app/shared/app-error-handler.ts
@@ -1,27 +1,33 @@
-import { EnvironmentProviders, ErrorHandler, Injectable, NgZone, inject, makeEnvironmentProviders } from '@angular/core';
-import { ToastService } from './toast';
 import { HttpErrorResponse } from '@angular/common/http';
+import {
+  EnvironmentProviders,
+  ErrorHandler,
+  Injectable,
+  NgZone,
+  inject,
+  makeEnvironmentProviders,
+} from '@angular/core';
+import { ToastService } from './toast';
 
 @Injectable({ providedIn: 'root' })
 export class AppErrorHandler extends ErrorHandler {
-    #toastService = inject(ToastService);
-    #zone = inject(NgZone);
+  #toastService = inject(ToastService);
+  #zone = inject(NgZone);
 
-    override handleError(error: unknown): void {
-        this.#zone.run(() => {
-            if (error instanceof HttpErrorResponse) {
-                this.#toastService.show('Error during backend communication.');
-            }
-            else {
-                this.#toastService.show('Unexpected error.');
-            }
-        })
-        super.handleError(error);
-    }
+  override handleError(error: unknown): void {
+    this.#zone.run(() => {
+      if (error instanceof HttpErrorResponse) {
+        this.#toastService.show('Error during backend communication.');
+      } else {
+        this.#toastService.show('Unexpected error.');
+      }
+    });
+    super.handleError(error);
+  }
 }
 
 export function provideAppErrorHandler(): EnvironmentProviders {
-    return makeEnvironmentProviders([
-        { provide: ErrorHandler, useClass: AppErrorHandler }
-    ]);
+  return makeEnvironmentProviders([
+    { provide: ErrorHandler, useClass: AppErrorHandler },
+  ]);
 }
