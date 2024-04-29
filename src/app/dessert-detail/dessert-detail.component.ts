d955473a9852f7fc6a33c767da07094db62dd23e
Manfred Steyer <manfred.steyer@gmx.net>
2024-04-29 09:46:19 +0200

    just show one option initially

--- src/app/dessert-detail/dessert-detail.component.ts
+++ src/app/dessert-detail/dessert-detail.component.ts
@@ -1,11 +1,9 @@
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Component, OnChanges, effect, inject, input, numberAttribute, untracked } from '@angular/core';
+import { Component, OnChanges, inject, input, numberAttribute } from '@angular/core';
 import { DessertDetailStore } from '../data/dessert-detail.story';
-import { explicitEffect } from '../shared/explicit-effect';
 import { JsonPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-dessert-detail',
@@ -26,40 +24,7 @@ export class DessertDetailComponent implements OnChanges {
   });
 
   ngOnChanges(): void {
-    // Option 1
-    // const id = this.id();
-    // this.store.load(id);
+    const id = this.id();
+    this.store.load(id);
   }
-
-  // Option 2
-  // constructor() {
-  //   this.store.rxLoad(this.id);
-  // }
-
-  // Option 3 - error demonstration
-  // constructor() {
-  //   // try to avoid this
-  //   effect(() => {
-  //     this.store.load(this.id());
-  //   });
-  // }
-
-  // Option 3
-  // constructor() {
-  //   // try to avoid this
-  //   effect(() => {
-  //     const id = this.id();
-  //     untracked(() => {
-  //       this.store.load(id);
-  //     });
-  //   });
-  // }
-
-  // Option 4
-  // constructor() {
-  //   explicitEffect(this.id, (id) => {
-  //     this.store.load(id);
-  //   });
-  // }
-
 }
