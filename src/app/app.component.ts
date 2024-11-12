e60c2c6afbd95935412e1502387505ff9579f4ee
Manfred Steyer <manfred.steyer@gmx.net>
2024-11-12 12:33:41 +0100

    fix: remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { DessertsComponent } from './desserts/desserts.component';
 import { ToastComponent } from './shared/toast';
 
@@ -8,7 +7,7 @@ import { ToastComponent } from './shared/toast';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, DessertsComponent, ToastComponent],
+  imports: [DessertsComponent, ToastComponent],
 })
 export class AppComponent {
   title = 'dessert';
