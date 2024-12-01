5828cc8a7c79e9a2a20312d4a6c3ebb547641492
Manfred Steyer <manfred.steyer@gmx.net>
2024-12-01 12:09:05 +0100

    add hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAppErrorHandler } from './shared/app-error-handler';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes,
       withComponentInputBinding()
     ),
-    provideHttpClient(),
+    provideHttpClient(
+      withFetch(),
+    ),
+    provideClientHydration(withEventReplay()),
     provideAppErrorHandler(),
   ],
 };
