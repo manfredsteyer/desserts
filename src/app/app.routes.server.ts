0643d20384a342118bf35c924300f30e090f3aed
Manfred Steyer <manfred.steyer@gmx.net>
2024-12-01 18:59:35 +0100

    showcase custom headers

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,11 +1,20 @@
 import { inject } from '@angular/core';
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { PrerenderFallback, RenderMode, ServerRoute } from '@angular/ssr';
 import { DessertService } from './data/dessert.service';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'desserts',
+    renderMode: RenderMode.Server,
+    headers: {
+      'X-Secret': 'Manfred was here!',
+    },
+    status: 201,
+  },
+  {
     path: 'desserts/:id',
     renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Client,
     async getPrerenderParams() {
       const dessertService = inject(DessertService);
       const ids = await dessertService.findIdsPromise();
